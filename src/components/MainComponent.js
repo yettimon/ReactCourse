5fe8a25e5e7bda2d69573b1ca88baebf532e9ed8
yettimon <globalobjectiveart@gmail.com>
2021-05-09 11:35:18 +0200

    adding featured section for home page

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,11 @@
 import { Navbar, NavbarBrand } from 'reactstrap'
 import Menu from './MenuComponent'
 import Home from './HomeComponent';
-import {DISHES} from '../shared/dishes'
+import Contact from './ContactComponent';
+import {DISHES} from '../shared/dishes';
+import {COMMENTS} from '../shared/comments';
+import {LEADERS} from '../shared/leaders';
+import {PROMOTIONS} from '../shared/promotions';
 import { Component } from 'react';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
@@ -13,15 +17,19 @@ class Main extends Component {
   constructor(props){
     super(props);
     this.state = {
-      dishes: DISHES
-
+      dishes: DISHES,
+      comments: COMMENTS,
+      leaders: LEADERS,
+      promotions: PROMOTIONS
     };
   }
 render(){
     const HomePage = () =>{
         return(
-
-        <Home/>
+        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
         );
     }
     return (
@@ -30,6 +38,7 @@ render(){
         <Switch>
             <Route path="/home" component={HomePage}/>
             <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} /> }/>
+            <Route exact path="/contactus" component={Contact}></Route>
             <Redirect to="/home"/>
         </Switch>
         <Footer/>
