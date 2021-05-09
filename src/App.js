1e1dab1fca7cb80f83acee52339dec0d90c3f9f8
yettimon <globalobjectiveart@gmail.com>
2021-05-09 11:02:53 +0200

    created spa from elements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import Main from './components/MainComponent'
 import { Component } from 'react';
+import {BrowserRouter} from 'react-router-dom'
 
 class App extends Component {
 
 render(){
   return (
+    <BrowserRouter>
     <div className="App">
     <Main/>
     </div>
+    </BrowserRouter>
   );
 }
 
